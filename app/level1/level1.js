01244bb68ef2bd558662054e8ce0fe4e4ad510b0
BenLaGrone <benjaminlagrone@gmail.com>
2016-02-24 08:43:48 -0600

    death star

--- app/level1/level1.js
+++ app/level1/level1.js
@@ -2,7 +2,64 @@ var level1 = {};
 
 level1.data;
 
+level1.parseAjax = function (xhr,id){
+    level1.data = JSON.parse(xhr.responseText);
+
+    document.getElementById('p0').innerHTML = '<i class="'+level1.data.objectgroups.moon.objects[0].idclass+' '+level1.data.objectgroups.moon.objects[0].sizeclass+' '+level1.data.objectgroups.moon.objects[0].colorclass+'"></i>';
+
+    var level1StarsHTML = '<div id="stars">';
+    for(i=0;i<level1.data.objectgroups.stars.objects.length;i++){
+        level1StarsHTML+='<i class="'+level1.data.objectgroups.stars.objects[i].idclass+' '+level1.data.objectgroups.stars.objects[i].colorclass+'"></i>';
+    }
+    level1StarsHTML += '</div>';
+    document.getElementById('p1').innerHTML=level1StarsHTML;
+    levels.spreadObjects(document.getElementById("stars").getElementsByTagName("i"),150,100,1,1,"fixed","%");
+
+    for (var key in level1.data.objectgroups.clouds){
+        var HTMLStart = '<div class="clouds">';
+        for(var key0 in level1.data.objectgroups.clouds[key].objects){
+            HTMLStart+='<i class="'+level1.data.objectgroups.clouds[key].objects[key0].idclass+' '+level1.data.objectgroups.clouds[key].objects[key0].sizeclass+' '+level1.data.objectgroups.clouds[key].objects[key0].colorclass+'"></i>';
+        }
+        HTMLStart+= '</div>';
+        document.getElementById(key).innerHTML=HTMLStart;
+    }
+    for (var i = 0; i < document.getElementsByClassName("clouds").length; i++){
+        levels.spreadObjects(document.getElementsByClassName("clouds")[i].getElementsByTagName("i"),window.innerHeight*.75,window.innerWidth*.75,1,1-(window.innerWidth/2),"relative","px");
+    }
+
+    var objectsHTMLStart = '';
+    for (var key in level1.data.objectgroups.objects){
+        objectsHTMLStart+='<div id="'+key+'">';
+        if(key==='rocket')
+            objectsHTMLStart+='<span>';
+        for(var key0 in level1.data.objectgroups.objects[key].objects){
+            objectsHTMLStart+='<i class="'+level1.data.objectgroups.objects[key].objects[key0].idclass+' '+level1.data.objectgroups.objects[key].objects[key0].sizeclass+' '+level1.data.objectgroups.objects[key].objects[key0].colorclass+'"></i>';
+        }
+        if(key==='rocket')
+            objectsHTMLStart+='</span>';
+        objectsHTMLStart+='</div>';
+    }
+    document.getElementById('objects').innerHTML=objectsHTMLStart;
+
+    var terraHTMLStart = '<div id="ground">';
+    for (var key in level1.data.objectgroups.terra){
+        terraHTMLStart+='<div class="'+key+'">';
+        for(var key0 in level1.data.objectgroups.terra[key].objects){
+            terraHTMLStart+='<i class="'+level1.data.objectgroups.terra[key].objects[key0].idclass+' '+level1.data.objectgroups.terra[key].objects[key0].sizeclass+' '+level1.data.objectgroups.terra[key].objects[key0].colorclass+'"></i>';
+        }
+        terraHTMLStart+='</div>';
+    }
+    terraHTMLStart+='</div>';
+    document.getElementById('terra').innerHTML=terraHTMLStart;
+
+    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("fa-tree"),0,14,-(window.innerHeight/28),1,"relative","px");
+    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("right")[0].getElementsByClassName("small"),0,14,-(window.innerHeight/13),1,"relative","px");
+    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("right")[0].getElementsByClassName("large"),0,14,-(window.innerHeight/15),1,"relative","px");
+};
+
 level1.updateElement = function() {
+
+    //TODO move to pattern
     if(Math.round(100*window.pageYOffset/(document.body.scrollHeight-document.documentElement.clientHeight))===0){
         window.location.hash = '#home';
         return;
@@ -16,6 +73,8 @@ level1.updateElement = function() {
     }
     if(document.body.scrollHeight-window.pageYOffset===document.documentElement.clientHeight)
         document.getElementById('curtain').className = 'fade';
+
+
 
     level1.getMovingElements(function (theObject,increment){
         theObject.style.position = "relative";
@@ -78,60 +137,7 @@ level1.getMovingElements = function(callback){
 };
 
 
-level1.parseAjax = function (xhr,id){
-    level1.data = JSON.parse(xhr.responseText);
 
-    document.getElementById('p0').innerHTML = '<i class="'+level1.data.objectgroups.moon.objects[0].idclass+' '+level1.data.objectgroups.moon.objects[0].sizeclass+' '+level1.data.objectgroups.moon.objects[0].colorclass+'"></i>';
-
-    var level1StarsHTML = '<div id="stars">';
-    for(i=0;i<level1.data.objectgroups.stars.objects.length;i++){
-        level1StarsHTML+='<i class="'+level1.data.objectgroups.stars.objects[i].idclass+' '+level1.data.objectgroups.stars.objects[i].colorclass+'"></i>';
-    }
-    level1StarsHTML += '</div>';
-    document.getElementById('p1').innerHTML=level1StarsHTML;
-    levels.spreadObjects(document.getElementById("stars").getElementsByTagName("i"),150,100,1,1,"fixed","%");
-
-    for (var key in level1.data.objectgroups.clouds){
-        var HTMLStart = '<div class="clouds">';
-        for(var key0 in level1.data.objectgroups.clouds[key].objects){
-            HTMLStart+='<i class="'+level1.data.objectgroups.clouds[key].objects[key0].idclass+' '+level1.data.objectgroups.clouds[key].objects[key0].sizeclass+' '+level1.data.objectgroups.clouds[key].objects[key0].colorclass+'"></i>';
-        }
-        HTMLStart+= '</div>';
-        document.getElementById(key).innerHTML=HTMLStart;
-    }
-    for (var i = 0; i < document.getElementsByClassName("clouds").length; i++){
-        levels.spreadObjects(document.getElementsByClassName("clouds")[i].getElementsByTagName("i"),window.innerHeight*.75,window.innerWidth*.75,1,1-(window.innerWidth/2),"relative","px");
-    }
-
-    var objectsHTMLStart = '';
-    for (var key in level1.data.objectgroups.objects){
-        objectsHTMLStart+='<div id="'+key+'">';
-        if(key==='rocket')
-            objectsHTMLStart+='<span>';
-        for(var key0 in level1.data.objectgroups.objects[key].objects){
-            objectsHTMLStart+='<i class="'+level1.data.objectgroups.objects[key].objects[key0].idclass+' '+level1.data.objectgroups.objects[key].objects[key0].sizeclass+' '+level1.data.objectgroups.objects[key].objects[key0].colorclass+'"></i>';
-        }
-        if(key==='rocket')
-            objectsHTMLStart+='</span>';
-        objectsHTMLStart+='</div>';
-    }
-    document.getElementById('objects').innerHTML=objectsHTMLStart;
-
-    var terraHTMLStart = '<div id="ground">';
-    for (var key in level1.data.objectgroups.terra){
-        terraHTMLStart+='<div class="'+key+'">';
-        for(var key0 in level1.data.objectgroups.terra[key].objects){
-            terraHTMLStart+='<i class="'+level1.data.objectgroups.terra[key].objects[key0].idclass+' '+level1.data.objectgroups.terra[key].objects[key0].sizeclass+' '+level1.data.objectgroups.terra[key].objects[key0].colorclass+'"></i>';
-        }
-        terraHTMLStart+='</div>';
-    }
-    terraHTMLStart+='</div>';
-    document.getElementById('terra').innerHTML=terraHTMLStart;
-
-    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("fa-tree"),0,14,-(window.innerHeight/28),1,"relative","px");
-    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("right")[0].getElementsByClassName("small"),0,14,-(window.innerHeight/13),1,"relative","px");
-    levels.spreadObjects(document.getElementById("ground").getElementsByClassName("right")[0].getElementsByClassName("large"),0,14,-(window.innerHeight/15),1,"relative","px");
-};
 
 if(id==='home'){
 
